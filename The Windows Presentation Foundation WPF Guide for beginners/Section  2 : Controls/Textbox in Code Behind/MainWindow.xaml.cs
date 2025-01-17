99c68ca6154284658c13d893ec55549ef896be97
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2025-01-17 16:06:32 +0530

    Add files via upload

---
+++ The Windows Presentation Foundation WPF Guide for beginners/Section  2 : Controls/Textbox in Code Behind/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Textblock_in_code_Behind
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TextBlock MyTb = new TextBlock();
+            MyTb.Text = "Hello World";
+            MyTb.Inlines.Add("This is added using Inlines");
+            MyTb.Inlines.Add(new Run("Run the code which code behinnd")
+            {
+                Foreground = Brushes.Red,
+                TextDecorations = TextDecorations.Underline
+            });
+            MyTb.TextWrapping = TextWrapping.Wrap;
+            MyTb.Foreground = Brushes.SeaGreen;
+            this.Content = MyTb;
+        }
+    }
+}
