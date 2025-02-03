71ccdd69c6279553db50631c41f9f75bf6a97780
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2025-02-03 15:24:17 +0530

    Add files via upload

---
+++ The Windows Presentation Foundation WPF Guide for beginners/Section  2 : Controls/15. Button & Some Events/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Button_and_Some_Events
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyLabel.Foreground = Brushes.OrangeRed;
+            
+        }
+
+        private void MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            double myfontsize = MyLabel.FontSize;
+            MyLabel.FontSize = myfontsize + 1;
+        }
+    }
+}
